421e5f320e838efa1b80ef285662012c92c972cf
rahatsikz <rascal6271@gmail.com>
2022-12-05 18:23:58 +0600

    loadnews added

--- js/app.js
+++ js/app.js
@@ -11,11 +11,19 @@ const displayCategory = (categories) => {
   categories.forEach((category) => {
     const newDiv = document.createElement("div");
     newDiv.classList.add("mt-2");
+    newDiv.classList.add("mouse-cursor");
     newDiv.innerHTML = `
-    <span onclick = "loadNews( ${category.category_id} )" > ${category.category_name} </span>
+    <span onclick = "loadNews( '${category.category_id}' )" > ${category.category_name} </span>
     `;
     categoryContainer.appendChild(newDiv);
   });
+};
+
+const loadNews = (id) => {
+  fetch(`https://openapi.programming-hero.com/api/news/category/${id} `)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error));
 };
 
 loadCategory();
